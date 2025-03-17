e9a61dfe162df4ca7f34059a20585262735a376e
Minh-Tri <nguyen.minhtri@outlook.fr>
2025-03-17 10:23:21 +0100

    Modification du nom de la variable

--- app/src/main/java/com/example/azurimmomn/model/Athlete.kt
+++ app/src/main/java/com/example/azurimmomn/model/Athlete.kt
@@ -1,10 +1,9 @@
 package com.example.azurimmomn.model
 
-import java.util.Date
 
 data class Athlete(
     val id: Int,
     val nom: String,
     val prenom: String,
-    val datenaiss: String,
+    val dateNaiss: String,
     )
