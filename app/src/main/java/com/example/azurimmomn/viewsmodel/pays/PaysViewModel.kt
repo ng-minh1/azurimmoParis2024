71ecfbd12c1f1732588e3b90c21269c3edcbdbbd
Minh-Tri <nguyen.minhtri@outlook.fr>
2025-03-17 10:48:48 +0100

    Modification du hardcoded en api

--- app/src/main/java/com/example/azurimmomn/viewsmodel/pays/PaysViewModel.kt
+++ app/src/main/java/com/example/azurimmomn/viewsmodel/pays/PaysViewModel.kt
@@ -1,30 +1,45 @@
 package com.example.azurimmomn.viewsmodel.pays
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.azurimmomn.model.Pays
+import androidx.lifecycle.viewModelScope
+import com.example.azurimmomn.api.RetrofitInstance
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class PaysViewModel : ViewModel() {
 
-    // Liste mutable des bâtiments
     private val _payss = MutableStateFlow<List<Pays>>(emptyList())
     val payss: StateFlow<List<Pays>> = _payss
 
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage: StateFlow<String?> = _errorMessage
+
     init {
-        // Simuler un chargement de données initiales
         getPays()
     }
 
-    // Fonction pour simuler le chargement de bâtiments
+
     private fun getPays() {
         viewModelScope.launch {
-            _payss.value = listOf(
-                Pays(1,  "Vietnam"),
-                Pays(2,  "France"),
-            )
+            _isLoading.value = true
+            _errorMessage.value = null  // Réinitialise l'erreur avant l'appel
+
+            try {
+                val response = RetrofitInstance.api.getPays()
+                _payss.value = response
+            } catch (e: Exception) {
+                _errorMessage.value = "Erreur : ${e.localizedMessage ?: "Une erreur s'est produite"}"
+            } finally {
+                _isLoading.value = false
+                println("Chargement terminé")
+            }
         }
     }
+
+
 }
