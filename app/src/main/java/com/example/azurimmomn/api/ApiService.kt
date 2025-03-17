b8c65de62bf8e7eb6dbe7c46c807ce4d62bcd507
Minh-Tri <nguyen.minhtri@outlook.fr>
2025-03-17 10:22:23 +0100

    ajout du getter getAthletes

--- app/src/main/java/com/example/azurimmomn/api/ApiService.kt
+++ app/src/main/java/com/example/azurimmomn/api/ApiService.kt
@@ -1,10 +1,14 @@
 package com.example.azurimmomn.api
 
 import com.example.azurimmomn.model.Actualite
+import com.example.azurimmomn.model.Athlete
 import retrofit2.http.GET
 
 
 interface ApiService {
     @GET("/actualites")
     suspend fun getActualites(): List<Actualite>
+
+    @GET("/athletes")
+    suspend fun getAthletes(): List<Athlete>
 }
