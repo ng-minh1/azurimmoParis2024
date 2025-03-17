a17757c4b74700c7dd91679d1ba94124f59f94a4
Minh-Tri <nguyen.minhtri@outlook.fr>
2025-03-17 10:35:49 +0100

    Ajout du getter de getEpreuves

--- app/src/main/java/com/example/azurimmomn/api/ApiService.kt
+++ app/src/main/java/com/example/azurimmomn/api/ApiService.kt
@@ -2,6 +2,7 @@ package com.example.azurimmomn.api
 
 import com.example.azurimmomn.model.Actualite
 import com.example.azurimmomn.model.Athlete
+import com.example.azurimmomn.model.Epreuve
 import retrofit2.http.GET
 
 
@@ -11,4 +12,7 @@ interface ApiService {
 
     @GET("/athletes")
     suspend fun getAthletes(): List<Athlete>
+
+    @GET("/epreuves")
+    suspend fun getEpreuves(): List<Epreuve>
 }
