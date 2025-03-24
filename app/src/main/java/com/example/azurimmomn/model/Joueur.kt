0b901e992a0c8b33e422597563d05e2a345ba2f8
Minh-Tri <nguyen.minhtri@outlook.fr>
2025-03-24 13:37:30 +0100

    Création du model Joueur

---
+++ app/src/main/java/com/example/azurimmomn/model/Joueur.kt
@@ -0,0 +1,12 @@
+package com.example.azurimmomn.model
+
+import java.time.LocalDate
+
+
+class Joueur {
+    val id: Int,
+    val nom: String,
+    val prenom: String,
+    val dateNaiss: LocalDate,
+
+}
